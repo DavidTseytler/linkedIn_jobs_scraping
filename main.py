0fcfb206d0304808cd2e3e0414aee13823c87918
David Tseytler <121279878+DavidTseytler@users.noreply.github.com>
2025-06-14 17:35:53 +1000

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ class LinkedInScraper:
                             link = card.find("a", class_="base-card__full-link")["href"].split("?")[0]
                             date = card.find("time", class_="job-search-card__listdate")
                             if date:
-                                date=date.get(datetime)
+                                date=date.get("datetime")
                             if not any(job.job_link == link for job in jobs):
                                 jobs.append(JobData(title, company, location_text, link, date, url))
                                 if len(jobs) >= max_jobs: break
